fff1494411ac71d7a4cdf147b7ae0e22d49d79cb
fragHorn <umesh.fractal@gmail.com>
2022-05-22 12:46:17 +0530

    Final changes

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -9,7 +9,6 @@ import DoctorPage from './DoctorPage/DoctorPage';
 import PatientPage from  './PaitentPage/PatientPage';
 import PatientSignup from '../Components/Login_Signup/Patients/signupP';
 
-
 function App() {
   return (
     <div className={styles.App}>
@@ -18,9 +17,9 @@ function App() {
         <Route path = "/doctor/login" element = {<DoctorLogin name = 'Doctor Login'/>} />
         <Route path = "/patient/login" element = {<PatientLogin name = 'Patient Login' />} />
         <Route path = "/doctor/all-patients/*" element = {<DoctorPage show = 'patients' heading = "Patient's List"/>} />
+        <Route path = "/doctor/appointments" element = {<DoctorPage show = 'appointments' heading = "Appointments"/>} />
         <Route path = "/patient/signup" element = {<PatientSignup />} />
-        <Route path = "/doctor/queue" element = {<DoctorPage show = 'schedule' heading = "Patient's Queue"/>} />
-        <Route path = "/doctor/appointments" element = {<DoctorPage show = 'Appointments' heading = "Appointments"/>} />
+        <Route path = "/doctor/queue" element = {<DoctorPage show = 'schedule' heading = 'Schedule'/>} />
         <Route path = "/patient/report" element = {<PatientPage show = 'report' heading = "Medical Report" />} />
         <Route path = "/patient/medicine" element = {<PatientPage show = 'medicine' heading = "Schedule Medicines" />} />
       </Routes>
